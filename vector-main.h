45812220fb08fc827147e4acd1286401a6fd74b8
Piotr Sawicki <01143606@pw.edu.pl>
2019-03-05 19:54:09 +0100

    1.4.4

--- vector-main.h
+++ vector-main.h
@@ -5,6 +5,7 @@
 void readValue(const Vector& vect, const unsigned int& n);
 void setValue(Vector& vect, const unsigned int& n);
 void loadVector(Vector& vect);
+unsigned int getOneOrTwo();
 void addVectors(Vector& v1, Vector& v2, Vector& result);
 void addVectors2(Vector& v1, Vector& v2);
 void subtractVectors(Vector& v1, Vector& v2, Vector& result);
